3566003a3896b1ba6f12398930649656fa3dce5d
unknown <rimueller@stud.hs-heilbronn.de>
2024-12-13 15:15:13 +0100

    added new files for testing purposes, but doesnt work for now

---
+++ Worker/Worker.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.Received += async (model, ea) =>
+{
+  Console.WriteLine($" [x] Received {message}");
+
+  int dots = message.Split('.').Length - 1;
+  await Task.Delay(dots * 1000);
+
+  Console.WriteLine(" [x] Done");
+};
+
+await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
