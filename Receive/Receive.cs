9cb2f0b924c9bdae74b397847e8c293558d9efda
unknown <rimueller@stud.hs-heilbronn.de>
2024-12-13 14:41:52 +0100

    Created the whole rabbitmq thingies and said 'hello world'

---
+++ Receive/Receive.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost"};
+using var conneciton = await factory.CreateConnectionAsync();
+using var channel = await conneciton.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine(" [*] Wating for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
